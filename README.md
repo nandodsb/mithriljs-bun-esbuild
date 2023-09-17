9bef80dc854ccf8f444c527745c77dfa52658e78
nandodsb <nandosbx@gmail.com>
2023-09-17 18:22:43 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# mithriljs-bun-esbuild
+
+Simple example of Mithriljs using bun, esbuild and tailwind.
+Esbuild Live Reload.
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun run build
+```
+
+This project was created using `bun init` in bun v1.0.0. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import m from 'mithril'
+
+new EventSource('/esbuild').addEventListener('change', () => location.reload())
+
+export const root = document.body
+
+let count = 0
+
+const App = {
+    view: function () {
+        return m("body.bg-gray-700.text-white.min-h-screen.h-full.flex.flex-1.gap-2.justify-center.items-center", [
+            m("h1.text-2xl", "My first app"),
+            m("button.bg-blue-400.hover:bg-blue-300.h-10.w-40", {
+                onclick: function () {
+                    count++
+                }
+            }, count + " clicks")
+        ])
+    }
+}
+
+m.mount(root, App)
