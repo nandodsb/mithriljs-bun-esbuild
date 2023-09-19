978ad693b323bb5fea4904b702b2d8840f165fa2
nandodsb <nandosbx@gmail.com>
2023-09-19 18:19:23 -0300

    Update

--- src/index.js
+++ src/index.js
@@ -2,19 +2,21 @@ import m from 'mithril'
 
 new EventSource('/esbuild').addEventListener('change', () => location.reload())
 
-export const root = document.body
+const root = document.body
 
 let count = 0
+const increment = function () {
+    count++
+}
 
 const App = {
+
     view: function () {
-        return m("body.bg-gray-700.text-white.min-h-screen.h-full.flex.flex-1.gap-2.justify-center.items-center", [
-            m("h1.text-2xl", "My first app"),
-            m("button.text-bold.text-mono.bg-red-500.hover:bg-red-300.h-10.w-40.rounded", {
-                onclick: function () {
-                    count++
-                }
-            }, count + " clicks")
+        return m("span.p-4.bg-gray-700.text-white.min-h-screen.flex.flex-1.justify-center.items-center.gap-2", [
+            m("h1.title.text-2xl", "My first app"),
+            m("button.text-bold.text-mono.bg-blue-500.hover:bg-blue-300.h-10.w-40.rounded", {
+                onclick: increment
+            }, count + " clicks"),
         ])
     }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [ "./src/**/*.{html,css,js}" ],
+  content: [ "./src/**/*.{html,css,js}",
+    './app/**/*.{html,css,js}',
+    './src/**/*.{html,css,js}'
+  ],
   theme: {
+    screens: {
+      'mobile': '400px',
+      'tablet': '640px',
+      'laptop': '1024px',
+      'desktop': '1280px',
+      'ultrawide': '1536px',
+    },
     extend: {},
   },
   plugins: [],
