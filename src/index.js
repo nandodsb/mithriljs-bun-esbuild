9b12d5af820c6fba51e7a3dd010fc1f40e5be38c
nandodsb <nandosbx@gmail.com>
2023-09-19 19:26:09 -0300

    Update

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const App = {
     view: function () {
         return m("span.p-4.bg-gray-700.text-white.min-h-screen.flex.flex-1.justify-center.items-center.gap-2", [
             m("h1.title.text-2xl", "My first app"),
-            m("button.text-bold.text-mono.bg-blue-500.hover:bg-blue-300.h-10.w-40.rounded", {
+            m("button.text-bold.text-mono.bg-red-500.hover:bg-red-300.h-10.w-40.rounded", {
                 onclick: increment
             }, count + " clicks"),
         ])
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,25 @@ export default {
   ],
   theme: {
     screens: {
-      'mobile': '400px',
-      'tablet': '640px',
-      'laptop': '1024px',
-      'desktop': '1280px',
-      'ultrawide': '1536px',
+      // 'tablet': '640px',
+      // 'laptop': '1024px',
+      // 'desktop': '1280px',
+      // 'ultrawide': '1536px',
+
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
     },
     extend: {},
   },
