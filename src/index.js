00d99189461fe6da507745d57942d1001c3b5ff9
nandodsb <nandosbx@gmail.com>
2023-09-19 22:35:43 -0300

    Update

--- src/index.js
+++ src/index.js
@@ -12,9 +12,11 @@ const increment = function () {
 const App = {
 
     view: function () {
-        return m("span", { class: "bg-gray-700 text-white min-h-screen flex grid grid-rows-2 justify-center items-center gap-2 sm:bg-blue-400    md:bg-teal-400 lg:bg-orange-400 xl:bg-violet-400 2xl:bg-black" }, [
-            m("h1.title.text-2xl", "My first app"),
-            m("button.text-bold.text-mono.bg-red-500.hover:bg-red-400.h-10.w-40.rounded.laptop:bg-green-400", {
+        return m("span.bg-gray-700.min-h-screen.flex.flex-col.justify-center.items-center.gap-2.text-white.font-sans.font-bold", {
+            class: "sm:bg-blue-400 sm:flex sm:flex-row sm:text-black md:bg-green-400 md:flex md:flex-col md:text-white lg:bg-orange-400 lg:flex lg:flex-row lg:text-black xl:bg-violet-400 xl:flex xl:flex-col xl:text-white 2xl:bg-black "
+        }, [
+            m("h1.text-center.text-2xl", "My first app"),
+            m("button.flex.justify-center.items-center.text-bold.text-mono.bg-red-500.hover:bg-red-400.h-10.w-40.rounded.laptop:bg-green-400", {
                 onclick: increment
             }, count + " clicks"),
         ])
